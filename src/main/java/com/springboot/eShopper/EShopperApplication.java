934da7f00cbfe01506435565a1eb5e862563fbbb
chinweibegbu <chinwe.ibegbu@gmail.com>
2021-12-02 15:28:22 +0100

    Initial commit

---
+++ src/main/java/com/springboot/eShopper/EShopperApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.eShopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopperApplication.class, args);
+	}
+
+}
