dd677186df69a3754f2c69c7d5724bf39f0a28b5
chinweibegbu <chinwe.ibegbu@gmail.com>
2021-12-08 08:56:47 +0100

    Created model repositories

---
+++ src/main/java/com/springboot/eShopper/Favourite/FavouriteRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.eShopper.Favourite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavouriteRepository extends JpaRepository<Favourite, Integer> {
+
+}
---
+++ src/main/java/com/springboot/eShopper/Shopper/ShopperRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.eShopper.Shopper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopperRepository extends JpaRepository<Shopper, Integer> {
+
+}
